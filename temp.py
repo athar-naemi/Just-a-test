3287e37a6baa4a69148f2e0d2edc0f453dd018d7
Athar Naemi | اطهر ناعمی <55176397+athar-naemi@users.noreply.github.com>
2023-03-03 14:45:19 +0330

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,41 @@
+#exapmle
+"""
+salammmm
+"""
+print('Hello world')
+# f9, f5
+a = 2
+b = 'a'
+c = True
+d = 2*4.5
+
+#list
+e = []
+
+f = [2,'a' , True, 4.5]
+
+#libariries
+import numpy as np #for arrays
+import pandas as pd #for dataframes
+import matplotlib.pyplot as plt #for plots
+# import sklearn #for machin learning
+# import nltk #for text mining
+# import seaborn #for plots
+# import math #for mathamatical functions
+
+#array
+g = np.array([1, 2, True])
+#araye hatma bayad toye beraket bashe
+
+g[2]
+# andis ha az 1 shoro mishe
+g[-3]
+h = pd.DataFrame({'c1':[1,2,3], 'c2':['a', 'b', 'c']})
+type(h)
+h.index = ['01', '02', '03']
+h.iloc[1 , 1]
+h. iloc [0:2, : ]
+h.loc [: , 'c1']
+
+#plot
+#files
